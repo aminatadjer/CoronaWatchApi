5124c7c471febcef66597b4d01f0a26f5e509633
aminatadjer <ga_tadjer@esi.dz>
2020-07-04 18:26:20 +0100

    solving conflicts

--- videos/models.py
+++ videos/models.py
@@ -5,15 +5,11 @@ from customauth.models import User
 
 
 class VideoInternaut(models.Model):
-    owner = models.ForeignKey(User, related_name='video', on_delete=models.CASCADE, default=1)
+    owner = models.ForeignKey(
+        User, related_name='video', on_delete=models.CASCADE, default=1)
     valide = models.BooleanField(default=False)
     supprime = models.BooleanField(default=False)
     vu = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
-<<<<<<< HEAD
     commentaire = models.CharField(max_length=20)
     media = models.FileField(null=True, default=None)
-=======
-    titre = models.CharField(max_length=20)
-    video = models.FileField()
->>>>>>> 9bbf2430d2cd45d2668bd465b30cd8cf8a85cd39
